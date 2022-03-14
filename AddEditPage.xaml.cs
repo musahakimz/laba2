07d4a50901ab08fdb8cd5b0d1747ed0ca86bd66a
musak <musak@HAKIMZYANOVMR>
2022-03-14 17:47:54 +0300

    Добавьте файлы проекта.

---
+++ AddEditPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laba2
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+         
+    {
+        private Employee _currentTable = new Employee();
+        public static TablePage tablePage;
+        private bool _flag = false;
+        public AddEditPage(Employee selectedTable, bool flag = false)
+        {
+            InitializeComponent();
+            if (selectedTable != null)
+                _currentTable = selectedTable;
+            DataContext = _currentTable;
+            _flag = flag;
+        }
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            
+
+            if (_flag)
+            {
+                KiyamobL5Entities.GetContext().Employee.Add(new Employee() { id = Convert.ToInt32(TbId.Text), FIO = TbFIO.Text, Position = Convert.ToInt32(TbPos.Text), Rank = Convert.ToInt32(TbRank.Text) });
+                KiyamobL5Entities.GetContext().SaveChanges();
+                tablePage.DGridTable.ItemsSource = KiyamobL5Entities.GetContext().Employee.ToList();
+                return;
+            }
+
+            try
+            {
+                KiyamobL5Entities.GetContext().SaveChanges();
+               
+                MessageBox.Show("Данные успешно добавлены!");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ TablePage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laba2
+{
+    /// <summary>
+    /// Логика взаимодействия для TablePage.xaml
+    /// </summary>
+    public partial class TablePage : Page
+    {
+        public TablePage()
+        {
+            InitializeComponent();
+            DGridTable.ItemsSource = KiyamobL5Entities.GetContext().Employee.ToList();
+            AddEditPage.tablePage = this;
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null, true));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var TableForRemoving = DGridTable.SelectedItems.Cast<Employee>().ToList();
+
+           
+                try
+                {
+                    KiyamobL5Entities.GetContext().Employee.RemoveRange(TableForRemoving);
+                    KiyamobL5Entities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены успешно!");
+                    DGridTable.ItemsSource = KiyamobL5Entities.GetContext().Employee.ToList();
+
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            
+        }
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Employee));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                KiyamobL5Entities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridTable.ItemsSource = KiyamobL5Entities.GetContext().Employee.ToList();
+            }
+        }
+    }
+
+}
+
